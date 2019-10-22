f6e729bcea0e0670b344db5b8cbdad279f6dff3d
Davin Alfarizky Putra Basudewa <dbasudewa@gmail.com>
2019-10-23 05:47:35 +0700

    DB respect to heroku database url

--- libs/database/db.go
+++ libs/database/db.go
@@ -8,6 +8,7 @@ import (
 	"database/sql"
 	"fmt"
 	_ "github.com/lib/pq"
+	"os"
 )
 
 //Connect ke database server
@@ -15,7 +16,14 @@ func Connect() *sql.DB {
 	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
 		"password=%s dbname=%s sslmode=disable",
 		config.Host, config.Port, config.Username, config.Password, config.DbName)
-	db, err := sql.Open("postgres", psqlInfo)
+	dbUrl := os.Getenv("DATABASE_URL")
+	var db *sql.DB
+	var err error
+	if dbUrl != "" {
+		db, err = sql.Open("postgres", dbUrl)
+	} else {
+		db, err = sql.Open("postgres", psqlInfo)
+	}
 	if db != nil {
 		err = db.Ping()
 	}
